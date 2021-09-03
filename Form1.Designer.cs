1fcdebc97a2ea0bbf985e96307e2c1955fc62c9e
Kody O'Neill <KONeill6@my.gcu.edu>
2021-09-03 18:35:17 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace CST_150_Activity_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MultipleRadioButton = new System.Windows.Forms.RadioButton();
+            this.DivideRadioButton = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.AddCheckBox = new System.Windows.Forms.CheckBox();
+            this.SubtractCheckBox = new System.Windows.Forms.CheckBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AddLabel = new System.Windows.Forms.Label();
+            this.SubtractLabel = new System.Windows.Forms.Label();
+            this.MultiDivideLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MultipleRadioButton
+            // 
+            this.MultipleRadioButton.AutoSize = true;
+            this.MultipleRadioButton.Location = new System.Drawing.Point(245, 214);
+            this.MultipleRadioButton.Name = "MultipleRadioButton";
+            this.MultipleRadioButton.Size = new System.Drawing.Size(60, 17);
+            this.MultipleRadioButton.TabIndex = 0;
+            this.MultipleRadioButton.TabStop = true;
+            this.MultipleRadioButton.Text = "Multiply";
+            this.MultipleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DivideRadioButton
+            // 
+            this.DivideRadioButton.AutoSize = true;
+            this.DivideRadioButton.Location = new System.Drawing.Point(381, 214);
+            this.DivideRadioButton.Name = "DivideRadioButton";
+            this.DivideRadioButton.Size = new System.Drawing.Size(55, 17);
+            this.DivideRadioButton.TabIndex = 1;
+            this.DivideRadioButton.TabStop = true;
+            this.DivideRadioButton.Text = "Divide";
+            this.DivideRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.listBox1.Location = new System.Drawing.Point(245, 247);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.listBox2.Location = new System.Drawing.Point(381, 247);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 3;
+            // 
+            // AddCheckBox
+            // 
+            this.AddCheckBox.AutoSize = true;
+            this.AddCheckBox.Location = new System.Drawing.Point(245, 176);
+            this.AddCheckBox.Name = "AddCheckBox";
+            this.AddCheckBox.Size = new System.Drawing.Size(45, 17);
+            this.AddCheckBox.TabIndex = 4;
+            this.AddCheckBox.Text = "Add";
+            this.AddCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SubtractCheckBox
+            // 
+            this.SubtractCheckBox.AutoSize = true;
+            this.SubtractCheckBox.Location = new System.Drawing.Point(381, 176);
+            this.SubtractCheckBox.Name = "SubtractCheckBox";
+            this.SubtractCheckBox.Size = new System.Drawing.Size(66, 17);
+            this.SubtractCheckBox.TabIndex = 5;
+            this.SubtractCheckBox.Text = "Subtract";
+            this.SubtractCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(245, 360);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(256, 45);
+            this.SubmitButton.TabIndex = 7;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Add:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Subtract:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Multiply/Divide:";
+            // 
+            // AddLabel
+            // 
+            this.AddLabel.AutoSize = true;
+            this.AddLabel.Location = new System.Drawing.Point(329, 51);
+            this.AddLabel.Name = "AddLabel";
+            this.AddLabel.Size = new System.Drawing.Size(0, 13);
+            this.AddLabel.TabIndex = 11;
+            // 
+            // SubtractLabel
+            // 
+            this.SubtractLabel.AutoSize = true;
+            this.SubtractLabel.Location = new System.Drawing.Point(329, 81);
+            this.SubtractLabel.Name = "SubtractLabel";
+            this.SubtractLabel.Size = new System.Drawing.Size(0, 13);
+            this.SubtractLabel.TabIndex = 12;
+            // 
+            // MultiDivideLabel
+            // 
+            this.MultiDivideLabel.AutoSize = true;
+            this.MultiDivideLabel.Location = new System.Drawing.Point(329, 115);
+            this.MultiDivideLabel.Name = "MultiDivideLabel";
+            this.MultiDivideLabel.Size = new System.Drawing.Size(0, 13);
+            this.MultiDivideLabel.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MultiDivideLabel);
+            this.Controls.Add(this.SubtractLabel);
+            this.Controls.Add(this.AddLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.SubtractCheckBox);
+            this.Controls.Add(this.AddCheckBox);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.DivideRadioButton);
+            this.Controls.Add(this.MultipleRadioButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton MultipleRadioButton;
+        private System.Windows.Forms.RadioButton DivideRadioButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.CheckBox AddCheckBox;
+        private System.Windows.Forms.CheckBox SubtractCheckBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AddLabel;
+        private System.Windows.Forms.Label SubtractLabel;
+        private System.Windows.Forms.Label MultiDivideLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            int s1 = Int32.Parse(listBox1.SelectedItem.ToString());
+            int s2 = Int32.Parse(listBox2.SelectedItem.ToString());
+
+            if (AddCheckBox.Checked)
+            {
+                AddLabel.Text = (s1 + s2).ToString();
+            }
+
+            if (SubtractCheckBox.Checked)
+            {
+                SubtractLabel.Text = (s1 - s2).ToString();
+            }
+
+            if (MultipleRadioButton.Checked)
+            {
+
+                MultiDivideLabel.Text = (s1 * s2).ToString();
+            }
+
+            if (DivideRadioButton.Checked)
+            {
+                MultiDivideLabel.Text = (s1 / s2).ToString();
+            }
+
+        }
+    }
+}
